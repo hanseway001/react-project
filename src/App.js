7649ea8288fcf6648cdc44d9e3c20f3641c31f08
Hanseway001 <wayne.hansen9276@stu.mtec.edu>
2023-04-11 09:08:43 -0600

    links are working

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
 // import './App.css';
 import { RouterProvider, createBrowserRouter} from 'react-router-dom';
-import Calendar from './PAGES/Calendar'
-import MyDay from './PAGES/MyDay'
-import Home from './PAGES/Home'
-import About from './PAGES/About'
+import Calendar from './pages/Calendar'
+import MyDay from './pages/MyDay'
+import Home from './pages/Home'
+import About from './pages/About'
 
 const router = createBrowserRouter([
   {
-    path: "/home",
+    path: "/",
     element: <Home />,
   },
   {
