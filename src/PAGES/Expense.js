ff6c2e618212e7d2d31edbf38eb9268b6d069bb7
Hanseway001 <wayne.hansen9276@stu.mtec.edu>
2023-04-14 10:02:00 -0600

    created add transaction functionality

--- src/PAGES/Expense.js
+++ src/PAGES/Expense.js
@@ -1,17 +1,42 @@
 import { styles } from "./Home";
+import { useState } from "react";
 
 export default function Expense({ handleBack }) {
+
+  const [inputValue, setInputValue] = useState(" ")
+  const [transactionArray, setTransactionArray] = useState([]);
+
+  const handleChange = (event) => {
+    const value = event.target.value;
+    setInputValue(value);
+  };
+
+  function handleAdd() {
+    setTransactionArray([...transactionArray, inputValue]);
+    setInputValue('');
+  }
+
+  function handleDelete() {
+
+  }
+
+  const transactionItems = transactionArray.map((transaction, index) =>{
+    return <div key={index}>{transaction}<button onClick={handleDelete}>Delete</button></div>
+  })
+
+
   return (
     <main>
       <h1>Expense!</h1>
       <div onClick={handleBack} style={styles.link}>
         Back to home
       </div>
-      <img
-        src="https://images.unsplash.com/photo-1619546813926-a78fa6372cd2?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1740&q=80"
-        height={500}
-        width={800}
-      />
+      <div>
+        <input type="text" placeholder="Add Transaction" value={inputValue} onChange={handleChange}></input>
+        <button onClick={handleAdd}>Add</button>
+      </div>
+
+        {transactionItems}
     </main>
   );
 }
